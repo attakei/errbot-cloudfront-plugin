5e626e6541c4a977c24dbf268ca8720598f892f1
Kazuya Takei <attakei@gmail.com>
2017-07-09 14:30:55 +0900

    Minimum command

--- cloudfront.py
+++ cloudfront.py
@@ -6,91 +6,19 @@ class Cloudfront(BotPlugin):
     Control CloudFront
     """
 
-    def activate(self):
-        """
-        Triggers on plugin activation
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        super(Cloudfront, self).activate()
-
-    def deactivate(self):
-        """
-        Triggers on plugin deactivation
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        super(Cloudfront, self).deactivate()
-
     def get_configuration_template(self):
         """
         Defines the configuration structure this plugin supports
 
         You should delete it if your plugin doesn't use any configuration like this
         """
-        return {'EXAMPLE_KEY_1': "Example value",
-                'EXAMPLE_KEY_2': ["Example", "Value"]
-               }
+        return {
+            'access_id': None,
+            'secret_key': None,
+        }
 
-    def check_configuration(self, configuration):
-        """
-        Triggers when the configuration is checked, shortly before activation
-
-        Raise a errbot.utils.ValidationException in case of an error
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        super(Cloudfront, self).check_configuration(configuration)
-
-    def callback_connect(self):
-        """
-        Triggers when bot is connected
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
-
-    def callback_message(self, message):
-        """
-        Triggered for every received message that isn't coming from the bot itself
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
-
-    def callback_botmessage(self, message):
-        """
-        Triggered for every message that comes from the bot itself
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
-
-    @webhook
-    def example_webhook(self, incoming_request):
-        """A webhook which simply returns 'Example'"""
-        return "Example"
-
-    # Passing split_args_with=None will cause arguments to be split on any kind
-    # of whitespace, just like Python's split() does
     @botcmd(split_args_with=None)
-    def example(self, message, args):
-        """A command which simply returns 'Example'"""
+    def cloudfront_list(self, message, args):
+        """Display knwon CloudFront edges"""
+        # TODO: Implement it
         return "Example"
-
-    @arg_botcmd('name', type=str)
-    @arg_botcmd('--favorite-number', type=int, unpack_args=False)
-    def hello(self, message, args):
-        """
-        A command which says hello to someone.
-
-        If you include --favorite-number, it will also tell you their
-        favorite number.
-        """
-        if args.favorite_number is None:
-            return "Hello {name}".format(name=args.name)
-        else:
-            return "Hello {name}, I hear your favorite number is {number}".format(
-                name=args.name,
-                number=args.favorite_number,
-            )
