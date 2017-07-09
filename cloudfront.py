4841366cae12b4a8f8c605746abf2a45186c4509
Kazuya Takei <attakei@gmail.com>
2017-07-09 15:17:18 +0900

    Refactor for flake8

--- cloudfront.py
+++ cloudfront.py
@@ -1,5 +1,5 @@
 import textwrap
-from errbot import BotPlugin, botcmd, arg_botcmd, webhook
+from errbot import BotPlugin, botcmd
 import boto3
 
 
@@ -13,33 +13,32 @@ class Cloudfront(BotPlugin):
             Please call `!plugin config cloudfront` to read format,
             And set your configurations.
             """
-        return textwrap.dedent(message)
+        return repr(textwrap.dedent(message))
 
     def _init_client(self):
-        return boto3.client('cloudfront',
+        """Return CloudFront client by boto3.
+
+        This client is configuret by plugin configuration.
+        """
+        return boto3.client(
+            'cloudfront',
             aws_access_key_id=self.config['access_id'],
             aws_secret_access_key=self.config['secret_key'],
         )
 
     def get_configuration_template(self):
         """
-        Defines the configuration structure this plugin supports
-
-        You should delete it if your plugin doesn't use any configuration like this
+        Defines the configuration structure this plugin supports.
         """
-        return {
+        config = {
             'access_id': None,
             'secret_key': None,
         }
+        return config
 
     @botcmd(split_args_with=None)
     def cloudfront_list(self, message, args):
-        """Display knwon CloudFront edges"""
-        # if not self.config:
-        #     return self._not_configured()
-        # if not self.config.get('access_id', None) \
-        #     or not self.config.get('secret_key', None):
-        #     return self._not_configured()
+        """Display knwon CloudFront edges."""
         if not self.config \
                 or not self.config.get('access_id', None) \
                 or not self.config.get('secret_key', None):
@@ -47,12 +46,15 @@ class Cloudfront(BotPlugin):
         client = self._init_client()
         result = client.list_distributions()
         if 'DistributionList' not in result:
-            return "Error"
-        quantity = result['DistributionList']['Quantity']
-        yield "Display {} items of distribution".format(quantity)
-        dists = result['DistributionList']['Items']
-        yield "\n".join([
-            "* {}: {}".format(dist['Id'] , dist['Origins']['Items'][0]['DomainName'])
-            for dist in dists
+            return "Client error"
+        result = result['DistributionList']
+        header = "Display {} items of distribution".format(result['Quantity'])
+        if result['Quantity'] == 0:
+            return header
+        dists = "\n".join([
+            "* {}: {}".format(
+                dist['Id'],
+                dist['Origins']['Items'][0]['DomainName'])
+            for dist in result['Items']
         ])
-        return
+        return "{}\n\n{}".format(header, dists)
